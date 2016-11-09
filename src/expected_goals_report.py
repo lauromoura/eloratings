8d5c0e6fc82f228fe780f4b5d8972490f5164add
Lauro Moura <lauromoura@gmail.com>
2016-11-09 12:41:02 -0300

    Split expected goals into another script

---
+++ src/expected_goals_report.py
@@ -0,0 +1,100 @@
+import sys
+import os
+import logging
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import defaultdict
+
+from elo.db import Championship
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv
+
+    try:
+        path = argv[1]
+    except IndexError as e:
+        logging.critical("You must provide a path")
+        sys.exit(1)
+
+    data = Championship.from_directory(argv[1])
+
+    standings = data.standings()
+    print(standings.sort_values(by='Points', ascending=False))
+
+    matches = data.pending_matches
+    matches = matches[matches['Round'] == 35]
+
+    attack = {}
+    defense = {}
+    diff = {}
+
+    for match in matches.itertuples():
+        home = standings.loc[match.HomeTeam]
+        away = standings.loc[match.AwayTeam]
+
+        print("Stats for ", match.HomeTeam, "vs", match.AwayTeam)
+
+        home_average_goals_for = home.HomeGoalsFor / home.HomeGames
+        home_average_goals_against = home.HomeGoalsAgainst / home.HomeGames
+        away_average_goals_for = away.AwayGoalsFor / away.AwayGames
+        away_average_goals_against = away.AwayGoalsAgainst / away.AwayGames
+
+        print("Home average goals for: %0.1f" % home_average_goals_for)
+        print("Home average goals against: %0.1f" % home_average_goals_against)
+        print("Away average goals for: %0.1f" % away_average_goals_for)
+        print("Away average goals against: %0.1f" % away_average_goals_against)
+
+        expected_home_for = home_average_goals_for * away_average_goals_against
+        expected_away_for = away_average_goals_for * home_average_goals_against
+        print("Expected home goals factor for: %0.1f" % expected_home_for)
+        print("Expected away goals factor for: %0.1f" % expected_away_for)
+        print("Expected TOTAL goals for: %0.1f" % (home_average_goals_for * away_average_goals_against + away_average_goals_for * home_average_goals_against))
+
+        print()
+
+        attack[match.HomeTeam] = expected_home_for
+        attack[match.AwayTeam] = expected_away_for
+        defense[match.HomeTeam] = expected_away_for
+        defense[match.AwayTeam] = expected_home_for
+        diff[match.HomeTeam] = expected_home_for - expected_away_for
+        diff[match.AwayTeam] = expected_away_for - expected_home_for
+
+
+    def sorted_dict_by_value(d, reverse=False):
+        return sorted(d, key=lambda k: d[k], reverse=reverse)
+
+    print("Best attacks")
+    for k in sorted_dict_by_value(attack, reverse=True):
+        print(k, attack[k])
+    print()
+
+    print("Best defenses")
+    for k in sorted_dict_by_value(defense):
+        print(k, defense[k])
+    print()
+
+    print("Best diff")
+
+    for k in sorted_dict_by_value(diff, reverse=True):
+        print(k, diff[k])
+
+    # print()
+    # ratings = data.load_initial_elo()
+
+    # latest = ratings.tail(1)
+    # print(pd.melt(latest).sort_values(by="value", ascending=False))
+    # plt.legend(loc=3)
+    # plt.show()
+
+    # print(data.standings())
+
+if __name__ == "__main__":
+    # import cProfile
+    # cProfile.run('main()', sort='cumtime')
+    main()
+
--- src/load_elo.py
+++ src/load_elo.py
@@ -23,52 +23,6 @@ def main(argv=None):
 
     data = Championship.from_directory(argv[1])
 
-    # print(data)
-    # print(data.matches)
-    # print(data.complete_matches)
-    # print(data.pending_matches)
-
-    # x = data.complete_matches
-    # print(x.groupby('HomeTeam'))
-
-    standings = data.standings()
-    print(standings.sort_values(by='Points', ascending=False))
-
-    matches = data.pending_matches
-    matches = matches[matches['Round'] == 34]
-
-    for match in matches.itertuples():
-        home = standings.loc[match.HomeTeam]
-        away = standings.loc[match.AwayTeam]
-
-        print("Stats for ", match.HomeTeam, "vs", match.AwayTeam)
-
-        home_average_goals_for = home.HomeGoalsFor / home.HomeGames
-        home_average_goals_against = home.HomeGoalsAgainst / home.HomeGames
-        away_average_goals_for = away.AwayGoalsFor / away.AwayGames
-        away_average_goals_against = away.AwayGoalsAgainst / away.AwayGames
-
-        # print("Home average goals for: ", home_average_goals_for)
-        # print("Home average goals against: ", home_average_goals_against)
-        # print("Away average goals for: ", away_average_goals_for)
-        # print("Away average goals against: ", away_average_goals_against)
-
-        print("Expected home goals for:", home_average_goals_for * away_average_goals_against)
-        print("Expected away goals for:", away_average_goals_for * home_average_goals_against)
-        print("Expected TOTAL goals for:", home_average_goals_for * away_average_goals_against + away_average_goals_for * home_average_goals_against)
-
-
-
-    print()
-    ratings = data.load_initial_elo()
-
-    # latest = ratings.tail(1)
-    # print(pd.melt(latest).sort_values(by="value", ascending=False))
-    # plt.legend(loc=3)
-    # plt.show()
-
-    print(data.standings())
-
     acc = defaultdict(list)
 
     for n in range(100000):
